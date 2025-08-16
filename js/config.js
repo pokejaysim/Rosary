ae4aeef64e441c305c7fad4b2bf3ef7bc7f8a290
pokejaysim <120335231+pokejaysim@users.noreply.github.com>
2025-08-16 09:25:28 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,17 +4,17 @@
 
 const config = {
     firebase: {
-        apiKey: process.env.FIREBASE_API_KEY || "AIzaSyDUpyuXLST_okNuDawiN3HTB2gvNmAa_u0",
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN || "rosary-companion-5fbb7.firebaseapp.com",
-        projectId: process.env.FIREBASE_PROJECT_ID || "rosary-companion-5fbb7",
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "rosary-companion-5fbb7.firebasestorage.app",
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "389748583800",
-        appId: process.env.FIREBASE_APP_ID || "1:389748583800:web:2f7c2973adf0b30af92c8d"
+        apiKey: "AIzaSyDUpyuXLST_okNuDawiN3HTB2gvNmAa_u0",
+        authDomain: "rosary-companion-5fbb7.firebaseapp.com",
+        projectId: "rosary-companion-5fbb7",
+        storageBucket: "rosary-companion-5fbb7.firebasestorage.app",
+        messagingSenderId: "389748583800",
+        appId: "1:389748583800:web:2f7c2973adf0b30af92c8d"
     },
     app: {
         name: "Rosary Companion",
         version: "1.0.0",
-        environment: process.env.NODE_ENV || "development"
+        environment: "development"
     }
 };
 
